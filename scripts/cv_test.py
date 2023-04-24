16aa52bcdd72b5c7c5802a84309a76b3f81554a4
weskerwork@gmail.com <weskerwork@gmail.com>
2023-04-24 21:32:09 +0800

    Update cv_test.py

--- scripts/cv_test.py
+++ scripts/cv_test.py
@@ -1,23 +1,47 @@
 #!/usr/bin/env python
 
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
 
+class ImageSubtraction:
+    def __init__(self):
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.image_callback)
+        self.background_image = cv2.imread("background.jpeg")
+
+    def image_callback(self, data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print e
+
+        difference = cv2.absdiff(self.background_image, cv_image)
+        gray = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+        dilated = cv2.dilate(thresh, None, iterations=3)
+        contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        if contours:
+            largest_contour = max(contours, key=cv2.contourArea)
+            closest_contour = min(contours, key=lambda cnt: cv2.pointPolygonTest(cnt, (data.width/2, data.height/2), True))
+
+            cv2.drawContours(cv_image, [largest_contour], -1, (0, 255, 0), 3)
+            cv2.drawContours(cv_image, [closest_contour], -1, (0, 0, 255), 3)
+
+            # Display the resulting image
+            cv2.imshow("Image Subtraction", cv_image)
+            cv2.waitKey(1)
+
 def main():
-    # Create a dummy image (a 400x400 black image)
-    img = np.zeros((400, 400, 3), dtype=np.uint8)
+    rospy.init_node("realsense_image_subtraction", anonymous=True)
+    image_subtraction = ImageSubtraction()
 
-    while True:
-        # Display the image in a window named "Test"
-        cv2.imshow("Test", img)
-
-        # Wait for a key press and exit the loop if 'q' is pressed
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
-
-    # Destroy all OpenCV windows
-    cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print ("Shutting down")
+    cv2.destroyAllWindows
